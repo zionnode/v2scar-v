eb6f2b203dfd2b34edc43f916c01891ae0b81481
Wei Zhang <zhangweihec@icloud.com>
2020-07-15 15:16:24 +0200

    fix

--- setup.sh
+++ setup.sh
@@ -8,12 +8,12 @@ dpkg --configure -a
 apt update -y && apt upgrade -y && apt install -y nginx python3 socat netcat curl wget python3-pip
 pip3 install requests
 
-cd ~ && mkdir .ssh && chmod 700 .ssh && touch ~/.ssh/authorized_keys
+mkdir /root/.ssh && chmod 700 .ssh && touch /root/.ssh/authorized_keys
 
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC/e1MctU7rZ0BmRzzFzxZK6UhUmfngBhRNUHdbihLqg6QyV4btEH7PcXw755B1pIodyEIJFBeYtkhSUVQ2evVkfp/gRjf/Gq2YWzGaFyvM+lHi6N2jchLf8hSSOBRh4NZyo7VjmJz3OuruBiBZLRaVXeQayIXA5Zd0IbKbd0i/sLVaa5Lf2vCk57EkMq/nEt5ajuCEI+AWtwFlu3/hmR1k7jFB49YnAQ/nHJBWY0wWelOtb7Emr6EAMHoyYIOqPa597qjMwAE/SqKtQuDUds73mszbjCXlIgXRmIAHOtd1RyYicXNq4tamk4Cfa0ApWxxkASTmip33R87iGXS6d/ij ilove0530@gmail.com\n" >> ~/.ssh/authorized_keys
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDA+xXDi3AtfMvpmP6cRtIbVzH7GS2KBik6Z6lnUvBerZ/MXVGDmirnlSiBK5TKpA+oTj1qNjihkbyYXpxZPFekFbw2vUdTx857vEGC35HF69yzmvu4TBEx8GGpxII4VBzEYcElTqKPL+bgk1EFSGUxAhdKDWZc2Z/pamOv8u9ms0IuW9Zn054z2/xjFxwJcOTJt6CRsYfxbViVX6IC4uMBLT9bRyY0PHo+TQX3LNDaJYl1lNvdPp6PrwKtNpt8B3oUhobKUifc08Zmgzld85+9Lvi7obW7TS5oCdSO0xmu6F8n6YXxIkiDk4TH1Y1y2te9s1s7AhCTynCSeT+c333J www@zion-core-site" >> ~/.ssh/authorized_keys
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDA+xXDi3AtfMvpmP6cRtIbVzH7GS2KBik6Z6lnUvBerZ/MXVGDmirnlSiBK5TKpA+oTj1qNjihkbyYXpxZPFekFbw2vUdTx857vEGC35HF69yzmvu4TBEx8GGpxII4VBzEYcElTqKPL+bgk1EFSGUxAhdKDWZc2Z/pamOv8u9ms0IuW9Zn054z2/xjFxwJcOTJt6CRsYfxbViVX6IC4uMBLT9bRyY0PHo+TQX3LNDaJYl1lNvdPp6PrwKtNpt8B3oUhobKUifc08Zmgzld85+9Lvi7obW7TS5oCdSO0xmu6F8n6YXxIkiDk4TH1Y1y2te9s1s7AhCTynCSeT+c333J www@zion-core-site" >> /root/.ssh/authorized_keys
 
-chmod 600 ~/.ssh/authorized_keys
+chmod 600 /root/.ssh/authorized_keys
 
 echo "PermitRootLogin prohibit-password" >> /etc/ssh/sshd_config
 echo "RSAAuthentication yes" >> /etc/ssh/sshd_config
@@ -47,20 +47,20 @@ echo "*            hard        nofile        1024000" >> /etc/security/limits.co
 
 echo "ulimit -SHn 1024000" >> /etc/profile
 
-cd ~
 curl  https://get.acme.sh | sh
 systemctl stop nginx
 ~/.acme.sh/acme.sh --issue -d "$URL" --standalone -k 2048
-~/.acme.sh/acme.sh --installcert -d "$URL" --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key
+~/.acme.sh/acme.sh --installcert -d "$URL" --fullchainpath /root/v2ray.crt --keypath /root/v2ray.key
 git clone https://zionnode:Zw19820130@github.com/zionnode/tempweb.git /var/www/tempweb
-
-cd ~
-git clone https://zionnode:Zw19820130@github.com/zionnode/v2scar-v.git
-cd v2scar-v
 
 python3 setup.py $URL
 
-cd ~
+rm -rf /etc/nginx/sites-available/https.conf
+rm -rf /etc/nginx/sites-enabled/https.conf
+ln -s /root/v2scar-v/https.conf /etc/nginx/sites-available/
+ln -s /etc/nginx/sites-available/https.conf /etc/nginx/sites-enabled/
+systemctl restart nginx
+
 touch mycron
 echo '58 0 * * * "/root/.acme.sh"/acme.sh --cron --home "/root/.acme.sh" > /dev/null' >> mycron
 crontab mycron
