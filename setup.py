fec9fa83a75244677ce2beab27b5950bd6faba8c
Wei Zhang <zhangweihec@icloud.com>
2020-07-15 12:51:40 +0200

    fix

--- setup.py
+++ setup.py
@@ -3,27 +3,31 @@ import os
 import sys
 import yaml
 
-os.system('echo "haha"')
+file_string = '''
+version: "3"
 
-dict_file = [
-    {'version':'3'},
-    {'services':
-        [
-            {
-                'v2ray': [
-                    'image',
-                    'container_name'
-                    ]
-            },
-            {
-                'web': [
-                    'image',
-                    'container_name'
-                ]
-            }
-            
-        ]
-    }
-    ]
-with open('/root/v2scar-v/docker-compose.yml', 'w+') as file:
-    yaml.dump(dict_file, file)
+services:
+    v2ray:
+        image: v2fly/v2fly-core:latest
+        container_name: v2ray
+        restart: always
+        volumes:
+            - ./v2ray-config.json:/etc/v2ray/config.json
+        ports: : 
+            - 20086:20086
+        env_file: 
+            - env.v2ray
+        command: ["v2ray","-config=http://34.89.127.171:8000/api/vmess_server_config/3717607258/?token=aWxvdmUwNTMwQGdtYWlsLmNvbSsxMDAwMQ%3D%3D"]
+
+    v2scar:
+        container_name: v2scar
+        image: ehco1996/v2scar
+        restart: always
+        depends_on:
+            - v2ray
+        links:
+            - v2ray
+        env_file: 
+            - env.v2scar
+'''
+os.system(f'echo "{file_string}" >> /root/v2scar-v/docker-compose.yml')
